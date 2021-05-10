607ca178636393207df145a52a6e9bf5ec53a269
lypintopimiento <lypintopimiento@gmail.com>
2021-05-10 18:16:04 -0500

    funciones_call_aplly_bing_21_04_2021

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+// declaración y expresiva. ECM
+
+let f1 = (callback) => callback()/2;
+let f2 = b => f3()-3;
+let f3 = c => 5 + 3;
+
+console.log(f1(f2));
+
+
+
+// let suma = (...arg) => {
+//     return arg;
+// }
+// console.log(suma(1,2));
+
+// function suma(a,b){
+//     return a + b;
+// }
+// let suma = function(a,b){
+//     return a + b;
+// }
+
+
+
+// callback
+// let f1 = function(callback){
+//     return callback();
+// }
+// let f2 = function(){
+//     console.log("Hola soy la funcion 1");
+//     return "Hola soy la funcion 2";
+// }
+// console.log(f1(f2));
+
+
+
+// Auto ejecutadas
+// (function (){
+//     console.log("Hola wfd");
+// })();
+
+
+
+// expresión
+// const nombre = function (...p1){
+//     console.log("Hola Mundo");
+//     return `Funcion dato ${p1}`
+// }
+
+// console.log(nombre("Ludwing"));
+// console.log(nombre("Camila"));
+
+
+// declaración.
+// function nombre(p1){
+//     console.log(p1);
+//     return `Ya se ejecuto la funcion`;
+// }
+
+// let resultado = nombre("ludwing");
+// console.log(resultado);
+
